30f20d77c445338cd8c21477053cad102ce655e0
Adieke <kayelumod04@gmail.com>
2024-11-09 12:09:44 +0800

    Add files via upload

---
+++ momo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner mop = new Scanner(System.in);
+        
+        int num = 5;
+        boolean momo = true;
+        
+        while(num > 5);
+        System.out.println("Enter a number: ");
+        num = mop.nextInt();
+        
+        
+        System.out.println("Reached 0");
+    }
+}
+
